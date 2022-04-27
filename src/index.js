14307f3f658fbb2cb298f1472a4f66ef9320aeba
Bishnu <bishnuthapako@gmail.com>
2022-04-27 19:31:05 -0400

    Completed assignment

--- src/index.js
+++ src/index.js
@@ -1,3 +1,18 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
+ document.querySelector("form").addEventListener("submit", (e) =>{
+   e.preventDefault()
+   handletodos(e.target.querySelector("#new-task-description").value)
+ })
+
 });
+
+
+function handletodos(todos){
+  let tasks = document.getElementById("tasks");
+  console.log(tasks);
+  const li = document.createElement("li");
+  li.textContent = todos;
+  tasks.appendChild(li)
+}
+
